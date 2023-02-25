22c4adbf477eadeb4b84f84630b8dcbf346c6146
Nasrullah <nasrullah01n@gmail.com>
2023-02-25 15:18:58 +0800

    Bluified the inputs
    Co-authored-by: Nitish <TechSupportz@users.noreply.github.com>

--- extension/src/content.ts
+++ extension/src/content.ts
@@ -1,1 +1,14 @@
-document.querySelector('body').innerHTML = 'Hello World!';
+let mutationObserver = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+        if (mutation.type === "childList") {
+            mutation.addedNodes.forEach(node => {
+                if (!node.parentElement.querySelector(".aoI")) return
+                const element = node.parentElement.querySelector(".aoI")
+                element.setAttribute("style", "background-color: blue;")
+                
+            })
+        }
+    })
+})
+
+mutationObserver.observe(document, { childList: true, subtree: true })
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,15 @@ import cors from "cors"
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+    res.send("Hello World!")
+
+    // res.json({ message: "Hello World!" })
 })
 
-app.listen(8080, () => {
-  console.log("Server started on http://localhost:8080")
-})
+const port = 8080
+app.listen(process.env.PORT || port, () =>
+    console.log(`Web server running @ http://localhost:${port}`),
+)
