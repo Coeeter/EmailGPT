acf10ed465ca4bec76bea6d2021df62967840377
Nasrullah <nasrullah01n@gmail.com>
2023-02-25 17:19:20 +0800

    Get text when click on btn

--- extension/src/content.ts
+++ extension/src/content.ts
@@ -1,32 +1,40 @@
-let mutationObserver = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
-        if (mutation.type === "childList") {
-            mutation.addedNodes.forEach(node => {
-                if (!node.parentElement.querySelector(".aoI")) return
-                const element = node.parentElement.querySelector(".aoI")
-                injectButton()
-                // element.setAttribute("style", "background-color: blue;")
-            })
-        }
+;(() => {
+    let mutationObserver = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            if (mutation.type === "childList") {
+                mutation.addedNodes.forEach(node => {
+                    if (!node.parentElement?.querySelector(".aoI")) return
+                    injectButton()
+                })
+            }
+        })
     })
-})
 
-const injectButton = () => {
-    const buttonRow = document.querySelectorAll(".btC")
+    const injectButton = () => {
+        const buttonRow = document.querySelectorAll(".btC")
 
-    const button = `
+        const button = `
     <td>
       <button class="email-gpt wG J-Z-I" style="margin-left: 12px;z-index:1 font-size: 20px">✨</button>
     </td>
     `
 
-    buttonRow.forEach(row => {
-        if (row.querySelector(".email-gpt")) return
-        row.querySelector(".gU.Up").insertAdjacentHTML("afterend", button)
-        row.querySelector(".email-gpt").addEventListener("click", () => {
-            console.log('Clicked')
+        buttonRow.forEach(row => {
+            if (row.querySelector(".email-gpt")) return
+            row.querySelector(".gU.Up").insertAdjacentHTML("afterend", button)
+            row.querySelector(".email-gpt").addEventListener("click", () => {
+                let parent = row.parentElement
+                while (true) {
+                    parent = parent.parentElement
+                    if (parent.nodeName == "TBODY") break
+                }
+                const text = parent.querySelector(
+                    'div[role="textbox"]',
+                ).innerHTML
+                console.log(text)
+            })
         })
-    })
-}
+    }
 
-mutationObserver.observe(document, { childList: true, subtree: true })
+    mutationObserver.observe(document, { childList: true, subtree: true })
+})()
