307a45b82a13e1a64905ee75846b7cbdb260ae37
Putt <57494734+PuttTim@users.noreply.github.com>
2023-02-26 02:38:23 +0800

    ⚡ Upped OpenAI's Max Tokens limit to 300

--- server/src/api/promptOpenAI.ts
+++ server/src/api/promptOpenAI.ts
@@ -14,7 +14,7 @@ const promptGPT = async (prompt: string) => {
         const completion = await openai.createCompletion({
             prompt: prompt,
             model: "text-davinci-003",
-            max_tokens: 200,
+            max_tokens: 300,
             temperature: 0.4,
             top_p: 1,
             frequency_penalty: 0,
