3ae5335d1ffa6c04d593b435371339427dac31b2
Nasrullah <nasrullah01n@gmail.com>
2023-02-25 17:03:42 +0800

    Injected button to all compose windows
    Co-authored-by: Nitish <TechSupportz@users.noreply.github.com>

--- extension/src/content.ts
+++ extension/src/content.ts
@@ -4,11 +4,29 @@ let mutationObserver = new MutationObserver(mutations => {
             mutation.addedNodes.forEach(node => {
                 if (!node.parentElement.querySelector(".aoI")) return
                 const element = node.parentElement.querySelector(".aoI")
-                element.setAttribute("style", "background-color: blue;")
-                
+                injectButton()
+                // element.setAttribute("style", "background-color: blue;")
             })
         }
     })
 })
+
+const injectButton = () => {
+    const buttonRow = document.querySelectorAll(".btC")
+
+    const button = `
+    <td>
+      <button class="email-gpt wG J-Z-I" style="margin-left: 12px;z-index:1 font-size: 20px">✨</button>
+    </td>
+    `
+
+    buttonRow.forEach(row => {
+        if (row.querySelector(".email-gpt")) return
+        row.querySelector(".gU.Up").insertAdjacentHTML("afterend", button)
+        row.querySelector(".email-gpt").addEventListener("click", () => {
+            console.log('Clicked')
+        })
+    })
+}
 
 mutationObserver.observe(document, { childList: true, subtree: true })
